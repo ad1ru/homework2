896f62318bdba52f25c2076177eabd9c5cd1310a
ad1ru <adiru04@gmail.com>
2023-12-05 00:37:44 +0600

    update homework

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
-import java.util.Random;
-
 public class Main {
-    public static int generateRandomAge(int a, int b) {
-        int random = a + (int) (Math.random() * b);
-        return random;
+    public static void main(String[] args) {
+        // System.out.println(generateRandomAge()); - чтобы выводил рандомное число в консоль.
+        ageAndTemperature(33, -10);
+        ageAndTemperature(10, 10);
+        ageAndTemperature(17, -1);
+        ageAndTemperature(46, -11);
+        ageAndTemperature(49, 21);
+
+        ageAndTemperature(generateRandomAge(), -20);
     }
 
-    public static void main(String[] args) {
-        int age = generateRandomAge(0, 100);
-        int temperature = -5;
-
-        // System.out.println(age); - чтобы выводил рандомное число в консоль.
-
+    public static void ageAndTemperature(int age, int temperature) {
         if ((age >= 20 && age <= 45) && (temperature >= -20 && temperature <= 30)) {
             System.out.println("Идем в парк");
         } else if (age < 20 && (temperature >= 0 && temperature <= 28)) {
             System.out.println("Идем в парк");
-        } else if (/*age > 45 || */(temperature >= -10 && temperature <= 25) && age > 45) {
+        } else if (age > 45 && (temperature >= -10 && temperature <= 25)) {
             System.out.println("Идем в парк");
         } else {
             System.out.println("Оставайтесь дома");
         }
+    }
+
+    public static int generateRandomAge() {
+        int random = 0 + (int) (Math.random() * 100);
+        return random;
     }
 }
